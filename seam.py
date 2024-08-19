1484302605505424f5797a4d1d88ead98b84187b
Pål Grønås Drange <Pal.Drange@uib.no>
2024-08-19 13:38:26 +0200

    Reuse energy map

--- seam.py
+++ seam.py
@@ -5,30 +5,24 @@ from tqdm import tqdm
 
 
 def compute_energy(img):
-    bw = img.convert("L")
-    gradient_x = bw.filter(ImageFilter.FIND_EDGES)
-    gradient_y = bw.filter(ImageFilter.FIND_EDGES)
-    energy = np.hypot(np.array(gradient_x), np.array(gradient_y))
+    bw = np.array(img.convert("L"))
+    gradient_x = np.abs(np.diff(bw, axis=1, prepend=bw[:, :1]))
+    gradient_y = np.abs(np.diff(bw, axis=0, prepend=bw[:1, :]))
+    energy = gradient_x + gradient_y
     return energy
 
 
 def compute_cumenergy(energy):
     height, width = energy.shape
-    cumenergy = energy.copy()
+    cumenergy = np.zeros((height, width))
+    cumenergy[0] = energy[0]
 
     for i in range(1, height):
-        for j in range(width):
-            if j == 0:
-                c_en = min(cumenergy[i - 1, j], cumenergy[i - 1, j + 1])
-            elif j == width - 1:
-                c_en = min(cumenergy[i - 1, j - 1], cumenergy[i - 1, j])
-            else:
-                c_en = min(
-                    cumenergy[i - 1, j - 1],
-                    cumenergy[i - 1, j],
-                    cumenergy[i - 1, j + 1],
-                )
-            cumenergy[i, j] += c_en
+        left_shift = np.roll(cumenergy[i - 1], 1)
+        right_shift = np.roll(cumenergy[i - 1], -1)
+        cumenergy[i] = energy[i] + np.minimum(
+            np.minimum(left_shift, cumenergy[i - 1]), right_shift
+        )
 
     return cumenergy
 
@@ -37,37 +31,40 @@ def find_vertical_seam(cumenergy):
     height, width = cumenergy.shape
     seam = np.zeros(height, dtype=int)
     seam[-1] = np.argmin(cumenergy[-1])
+
     for i in range(height - 2, -1, -1):
         j = seam[i + 1]
         if j == 0:
-            seam[i] = np.argmin(cumenergy[i, j : j + 2])
+            idx = np.argmin(cumenergy[i, j : j + 2])
         elif j == width - 1:
-            seam[i] = np.argmin(cumenergy[i, j - 1 : j + 1]) + j - 1
+            idx = np.argmin(cumenergy[i, j - 1 : j + 1]) - 1
         else:
-            seam[i] = np.argmin(cumenergy[i, j - 1 : j + 2]) + j - 1
+            idx = np.argmin(cumenergy[i, j - 1 : j + 2]) - 1
+        seam[i] = j + idx
+
     return seam
 
 
 def remove_seam(img, seam):
-    width, height = img.size
-    new_img = Image.new("RGB", (width - 1, height))
+    height, width, channels = img.shape
+    new_img = np.zeros((height, width - 1, channels), dtype=img.dtype)
+
     for i in range(height):
-        k = 0
-        for j in range(width):
-            if j != seam[i]:
-                new_img.putpixel((k, i), img.getpixel((j, i)))
-                k += 1
+        new_img[i, :, :] = np.delete(img[i, :, :], seam[i], axis=0)
+
     return new_img
 
 
 def seam_carving(img, new_width):
     orig_width = img.width
+    img = np.array(img)
     for _ in tqdm(range(new_width, orig_width)):
-        energy = compute_energy(img)
+        energy = compute_energy(Image.fromarray(img))
         cumenergy = compute_cumenergy(energy)
         seam = find_vertical_seam(cumenergy)
         img = remove_seam(img, seam)
-    return img
+
+    return Image.fromarray(img)
 
 
 if __name__ == "__main__":
@@ -77,5 +74,6 @@ if __name__ == "__main__":
     width = int(sys.argv[2])
     input_img = Image.open(fname)
     resized_img = seam_carving(input_img, new_width=width)
+    resized_img = resized_img.convert("RGB")
     fname_ = fname.split(".")[0]
     resized_img.save(fname_ + f"-{width}.jpg")
